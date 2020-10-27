0c3e74e4ac75bbfb82ffd3c18c775a086067a332
Arielle Mueller <arielle_jaqueline@hotmail.com>
2020-10-27 12:54:31 -0400

    second commit

---
+++ ariellelab3/src/main/java/arielle/mueller/m301037045/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package arielle.mueller.m301037045.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
